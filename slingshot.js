5328270b8ef985d59525ffb10fc504b9878a2638
Jassi <jassi@Jassis-MacBook-Air.local>
2021-04-26 15:46:31 +0530

    Slingshot

---
+++ slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+fly(){
+    Matter.Body.applyForce(this.sling.bodyA,this.sling.bodyA.position,{x:this.pointB.x,y:this.pointB.y})
+    this.sling.bodyA = null;
+}
+
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+}
+}
