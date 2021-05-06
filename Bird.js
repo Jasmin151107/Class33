fc5d54aea4c041775f9ac265b82d59483b3dfb57
Jassi <jassi@Jassis-MacBook-Air.local>
2021-05-06 19:49:54 +0530

    Trajectory

--- Bird.js
+++ Bird.js
@@ -1,12 +1,27 @@
 class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
+    this.visibility =255;
     this.image = loadImage("sprites/bird.png");
+    this.smokeImg = loadImage("sprites/smoke.png");
+    this.trajectory = [];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+    if(this.body.velocity.x>10 && this.body.position.x >200){
+    var position = [this.body.position.x,this.body.position.y];
+    this.trajectory.push(position);
+  }
     super.display();
+
+    for (var i=0; i<this.trajectory.length;i++){
+      push()
+      this.visibility = this.visibility-0.5
+      tint (255,this.visibility);
+      image(this.smokeImg,this.trajectory[i][0],this.trajectory[i][1])
+      pop()
+    }
   }
 }
